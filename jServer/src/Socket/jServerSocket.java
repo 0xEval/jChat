971312fd2f6adafd9a6a5efc019147ab321b4d18
jcouvy <julien.couvy@gmail.com>
2018-02-12 11:32:58 +0700

    Sample scheduled executor writing to JSON object

--- jServer/src/Socket/jServerSocket.java
+++ jServer/src/Socket/jServerSocket.java
@@ -5,18 +5,31 @@
  * Project: jChat - TCP/UDP based multi-threaded chat application w/ GUI
  * Licence: GPLv3
  * GitHub:  https://github.com/0xEval/jChat
+ *
+ * Description:
+ *  Initializes the server socket on a given port and hostname.
+ *  Each incoming connection is handled by a separate executor ClientHandler.
+ *  All handlers are kept up-to-date in a hash set server-side.
+ *
  * --------------------------------------------------------------------- */
 
 package Socket;
 
 import java.io.IOException;
 
+import java.io.StringWriter;
 import java.net.Socket;
 import java.net.ServerSocket;
 
+import java.text.SimpleDateFormat;
 import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+// No JSON API is not bundled by default in Java (See: https://javaee.github.io/jsonp/).
+import javax.json.*;
 
 public class jServerSocket {
 
@@ -39,7 +52,26 @@ public class jServerSocket {
                 + "\nWaiting for connections"
         );
 
-        // Creating a new listener (ie: executor thread) for each incoming client connection on the server.
+        // Creates a scheduled executor in charge of logging the chat history into a JSON file every X time units.
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                JsonObject model = Json.createObjectBuilder()
+                        .add("user", "Duke")
+                        .add("date", new SimpleDateFormat().toString())
+                        .add("content", "PLACEHOLDER AyyyLMAO")
+                        .build();
+
+                StringWriter strWriter = new StringWriter();
+                JsonWriter   jsnWriter = Json.createWriter(strWriter);
+                jsnWriter.writeObject(model);
+                jsnWriter.close();
+                System.out.println(strWriter.toString());
+            }
+        }, 0, 5, TimeUnit.SECONDS);
+
+        // Creates a new listener (ie: executor thread) for each incoming client connection on the server.
         while (true) {
             try {
                 Socket client = socket.accept();
@@ -48,7 +80,8 @@ public class jServerSocket {
                 pool.execute(newClient);
                 clientList.add(newClient);
                 for (ClientHandler c : clientList) {
-                    c.updateList(newClient);
+                    c.updateClientList(clientList);
+                    c.sendClientList();
                 }
             } catch (IOException io) {
                 io.printStackTrace();
