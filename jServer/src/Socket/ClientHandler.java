3e44a9d59a7eb4683b9a048cd5eff58453d5227a
jcouvy <julien.couvy@gmail.com>
2018-02-20 21:30:49 +0700

    Message based communication + login ack

--- jServer/src/Socket/ClientHandler.java
+++ jServer/src/Socket/ClientHandler.java
@@ -9,59 +9,122 @@
 
 package Socket;
 
+import java.io.*;
 import java.net.Socket;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 import java.util.HashSet;
 
+import Data.HeaderList;
+import Data.Message;
+
 public class ClientHandler implements Runnable {
 
-    private final Socket         client;
-    private final BufferedReader reader;
-    private final BufferedWriter writer;
+    private final jServerSocket server;
+    private final Socket        client;
+    private boolean             connected;
+    private String              username;
+    private ObjectInputStream   reader;
+    private ObjectOutputStream  writer;
 
     private HashSet<ClientHandler> clientList;
 
-    public ClientHandler(Socket socket, HashSet<ClientHandler> list) throws IOException {
+    public ClientHandler(jServerSocket ssocket, Socket socket, HashSet<ClientHandler> list) throws IOException {
+        server     = ssocket;
         client     = socket;
-        reader     = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        writer     = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        connected  = false;
+        writer     = new ObjectOutputStream(socket.getOutputStream());
+        reader     = new ObjectInputStream(socket.getInputStream()) ;
         clientList = list;
     }
 
-    public void updateList(ClientHandler ch) {
-        if (!ch.equals(this)) {
-            clientList.add(ch);
-            System.out.println("\t\tDEBUG: Updating client list [#" + Thread.currentThread().getId() + "]");
-        }
+    public void updateClientList(HashSet<ClientHandler> list) {
+        clientList = list;
     }
 
-    private void writeMessage(String msg) {
+    public void sendClientList() {
+        StringBuilder sb = new StringBuilder("");
+        for (ClientHandler c : clientList) {
+            sb.append(c.username);
+            sb.append(":");
+        }
+        System.out.println("DEBUG: Client List " + sb);
+        Message m = new Message(HeaderList.UPD, "SERVER", sb.toString());
+        writeMessage(m);
+    }
+
+    private void writeMessage(Message msg) {
         try {
-            writer.write(msg);
-            writer.newLine();
-            writer.flush();
+            writer.writeObject(msg);
+            if (msg.getHeader() == HeaderList.MSG)
+                server.logMessage(msg);
         } catch (IOException io) { io.printStackTrace(); }
+    }
+
+    private boolean checkUsernameValidity(String username) {
+        for (ClientHandler ch : clientList) {
+            if (ch.username.equals(username)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public void run() {
+
         System.out.println("\tStarting a new listener on Thread #" + Thread.currentThread().getId());
+        Message msg;
+        int loginAttempts = 0;
+
         try {
-            String inputLine;
-            while ((inputLine = reader.readLine()) != null) {
-                for (ClientHandler ch : clientList) ch.writeMessage(inputLine);
-                System.out.println(
-                        "\tReceived message \"" + inputLine + "\" from Thread #" + Thread.currentThread().getId());
+
+            while (!connected) {
+                msg = (Message) reader.readObject();
+                if (msg.getHeader() == HeaderList.LOG) {
+                    if (checkUsernameValidity(msg.getData())) {
+                        connected = true;
+                        username = msg.getData();
+                        server.getClientList().add(this);
+                        clientList.add(this);
+                        sendClientList();
+                        this.writeMessage(new Message(HeaderList.LOG, "SERVER", "OK"));
+                        for (ClientHandler ch : clientList)
+                            ch.writeMessage(new Message(HeaderList.MSG, "SERVER", "User " +
+                                    username + " has joined the room."));
+                    } else {
+                        System.out.println("loginattemps++");
+                        loginAttempts++;
+                    }
+                }
+                if (loginAttempts >= 3) {
+                    server.getClientList().remove(this);
+                    System.err.println("Connection reset on Thread #"+Thread.currentThread().getId());
+                    Thread.currentThread().interrupt();
+                    System.exit(-1);
+                }
             }
-        } catch (IOException io) {
-            io.printStackTrace();
+
+            while ((msg = (Message) reader.readObject()) != null) {
+                switch (msg.getHeader()) {
+                   default:
+                        break;
+                   case HeaderList.MSG:
+                        for (ClientHandler ch : clientList)
+                            ch.writeMessage(msg);
+                        System.out.println("\tReceived message \"" + msg.getData() + "\" from Thread #" + Thread.currentThread().getId());
+                        break;
+                   case HeaderList.PRV:
+                       for (ClientHandler ch : clientList)
+                           // Only the sender and receiver are able to see the private message
+                           if (ch.username.equals(msg.getDest()) || ch.username.equals(msg.getSender()))
+                               ch.writeMessage(msg);
+                       break;
+                }
+            }
+
         }
+        catch (IOException io) { io.printStackTrace(); }
+        catch (ClassNotFoundException cnfe) { cnfe.printStackTrace(); }
     }
 
 }
--- jServer/src/Socket/jServerSocket.java
+++ jServer/src/Socket/jServerSocket.java
@@ -15,21 +15,21 @@
 
 package Socket;
 
+import Data.History;
+
 import java.io.IOException;
 
-import java.io.StringWriter;
 import java.net.Socket;
 import java.net.ServerSocket;
 
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-// No JSON API is not bundled by default in Java (See: https://javaee.github.io/jsonp/).
-import javax.json.*;
+import Data.Message;
 
 public class jServerSocket {
 
@@ -38,11 +38,21 @@ public class jServerSocket {
     private final int              poolSize = 16;
 
     private HashSet<ClientHandler> clientList;
+    protected ArrayList<Message> msgBuffer;
 
     public jServerSocket(int portNumber) throws IOException {
         socket     = new ServerSocket(portNumber);
         clientList = new HashSet<ClientHandler>();
         pool       = Executors.newFixedThreadPool(poolSize);
+        msgBuffer  = new ArrayList<Message>();
+    }
+
+    protected HashSet<ClientHandler> getClientList() {
+        return clientList;
+    }
+
+    public synchronized void logMessage(Message msg) {
+       msgBuffer.add(msg);
     }
 
     public void run() {
@@ -53,32 +63,28 @@ public class jServerSocket {
         );
 
         // Creates a scheduled executor in charge of logging the chat history into a JSON file every X time units.
+        History.createHistory();
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleWithFixedDelay(new Runnable() {
             @Override
             public void run() {
-                JsonObject model = Json.createObjectBuilder()
-                        .add("user", "Duke")
-                        .add("date", new SimpleDateFormat().toString())
-                        .add("content", "PLACEHOLDER AyyyLMAO")
-                        .build();
-
-                StringWriter strWriter = new StringWriter();
-                JsonWriter   jsnWriter = Json.createWriter(strWriter);
-                jsnWriter.writeObject(model);
-                jsnWriter.close();
-                System.out.println(strWriter.toString());
+                System.out.println("Logging chat history...");
+                System.out.println(msgBuffer);
+                if (!msgBuffer.isEmpty())
+                    for (Message m : msgBuffer)
+                        History.logMessage(m);
+                    msgBuffer.clear();
             }
-        }, 0, 5, TimeUnit.SECONDS);
+        }, 60, 60, TimeUnit.SECONDS);
 
         // Creates a new listener (ie: executor thread) for each incoming client connection on the server.
         while (true) {
             try {
                 Socket client = socket.accept();
                 System.out.println("Incoming connection from " + client.getLocalSocketAddress());
-                ClientHandler newClient = new ClientHandler(client, clientList);
+                ClientHandler newClient = new ClientHandler(this, client, clientList);
                 pool.execute(newClient);
-                clientList.add(newClient);
+                // Send the updated list to each executor
                 for (ClientHandler c : clientList) {
                     c.updateClientList(clientList);
                     c.sendClientList();
